d972da8c4230115304d2e8896a62a43a80ddd8aa
vandhear <wulf.sheleg@gmail.com>
2018-07-02 13:26:11 +0300

    Test

---
+++ app/src/main/java/com/example/ronik/myticketproject1/MyTextView.java
@@ -0,0 +1,7 @@
+package com.example.ronik.myticketproject1;
+
+import android.widget.TextView;
+
+//public class MyTextView extends TextView {
+  //  boolean isCorrectAnswer;
+//}
---
+++ app/src/main/java/com/example/ronik/myticketproject1/OneTableRawData.java
@@ -0,0 +1,10 @@
+package com.example.ronik.myticketproject1;
+
+public class OneTableRawData {
+    String correctAnswer;
+    String wrongAnswer0;
+    String wrongAnswer1;
+    String wrongAnswer2;
+    String wrongAnswer3;
+}
+
